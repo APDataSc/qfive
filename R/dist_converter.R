54b104a0215203acdcdfdd9b173d365b6f2053ba
APDataSc <sklan8@hotmail.com>
2025-10-17 14:20:59 -0600

    Initial commit

---
+++ R/dist_converter.R
@@ -0,0 +1,28 @@
+#' Convert between distances
+#'
+#' Performs the conversion based on specified `unit_from` and `unit_to` values.
+#'
+#' @param dist_value A numerical distance value to be converted.
+#' @param unit_from A character string of the distance unit to convert from
+#' @param unit_to A character string of the distance unit to convert to
+# Add returning value description and tag
+#' @return A numeric distance value in the unit specified as `unit_to`.
+# Export this function
+#' @export
+
+dist_converter <-
+function(dist_value,
+                           unit_from,
+                           unit_to) {
+  # Fill in the missing parts of the if-else if-else flow
+  if (unit_from == "feet" && unit_to == "meters") {
+    return(dist_value * 0.3048)
+  } else if (unit_from == "meters" && unit_to == "feet") {
+    return(dist_value * 3.2808)
+  } else if (unit_from == unit_to) {
+    warning("unit_from and unit_to are the same, returning dist_value")
+    return(dist_value)
+  } else {
+    warning("This function only supports conversions between feet and meters.")
+  }
+}
