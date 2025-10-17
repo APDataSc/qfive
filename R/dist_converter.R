535cc5713f3e60d8d2ee4e2d57d267119c4d832d
APDataSc <sklan8@hotmail.com>
2025-10-17 14:38:00 -0600

    Initial commit

--- R/dist_converter.R
+++ R/dist_converter.R
@@ -26,3 +26,18 @@ function(dist_value,
     warning("This function only supports conversions between feet and meters.")
   }
 }
+
+
+#' Distance values and units
+#'
+#' Distance values and corresponding unit (feet, meters, or inches)
+# Update format
+#' @format Data frame with two columns and 1000 rows.
+# Document column names and information
+#' \describe{
+#' \item{value}{Numeric distance value.}
+#' \item{unit}{Distance unit.}
+#' }
+# Add example and string of data name
+#' @examples
+#' distance_data
